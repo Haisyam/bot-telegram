c8601c74dcb04579dbacb3fb36b8e746aae3c5af
Muhamad Haisyam Khairizmi <81277845+Haisyam@users.noreply.github.com>
2024-05-12 11:37:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,21 +2,21 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // TOKEN BOT TELEGRAM
-global.token = ["6136209053:AAE2nhENFUk9eLkXQp8X1Qg17XoDTMq1Lvo"]
+global.token = ["6099209702:AAHTdqC6d9OUiJMXs_CnTDsJ9-jsTPmMvOM"]
 // BOTNAME
-global.botname = 'HLXEVO'
+global.botname = 'JurigBOT'
 
 //APIKEY XCODERS
-global.xcoders = 'MoPCbxiToSFepcB'
+global.xcoders = 'pxp7YYCE7D8J9pU'
 
 //APIKET RSNCHAT 
-global.rsnchat = 'rsnai_w3Fdj3wh0SrRs118UsBkjSRN'
+global.rsnchat = 'rsnai_65hummaIA8Pl4EeOyPLZf0iW'
 
 //MONGODBURL 
 global.MONGODB_URI = 'mongodb+srv://nuxysapi:lelang18@cluster0.cmvqrmm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 //OTHER 
-global.owner = 'ahmadzakiyo'
+global.owner = 'JurigVPN'
 global.wait = '⏳ Mohon tunggu sebentar'
 
 let file = require.resolve(__filename)
